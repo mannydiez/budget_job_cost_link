8ae43b8cfc09c550f8056af55958b1a0bef9d6bb
diez43011 <diez43011@gmail.com>
2019-04-23 15:36:41 +0800

    d

--- models/budget_job.py
+++ models/budget_job.py
@@ -22,8 +22,8 @@ class job_costing_planned_amount_comparison(models.Model):
 			if list_of_objects:
 				for rec in list_of_objects:
 					for rec2 in vals['job_labour_line_ids']:
-						if rec['group_product_id'] == rec2['group_product_id']:
-							rec['total_cost'] += rec2['total_cost']
+						if rec.get('group_product_id') == rec2.get('group_product_id'):
+							rec.get('total_cost') += rec2.get('total_cost')
 			else:
 				list_of_objects = vals['job_labour_line_ids']
 			log.critical('list_of_objects = {}'.format(list_of_objects))
@@ -32,21 +32,21 @@ class job_costing_planned_amount_comparison(models.Model):
 			if list_of_objects:
 				for rec in list_of_objects:
 					for rec2 in vals['job_subcon_line_ids']:
-						if rec['group_product_id'] == rec2['group_product_id']:
-							rec['total_cost'] += rec2['total_cost']
+						if rec.get('group_product_id') == rec2.get('group_product_id'):
+							rec.get('total_cost') += rec2.get('total_cost')
 			else:
 				list_of_objects = vals['job_subcon_line_ids']
 			
 			log.critical('list_of_objects = {}'.format(list_of_objects))
 		if vals.get('job_overhead_line_ids'):
+			log.critical('job_overhead_line_ids = {}'.format(vals['job_overhead_line_ids']))
 			if list_of_objects:
 				for rec in list_of_objects:
 					for rec2 in vals['job_overhead_line_ids']:
-						if rec['group_product_id'] == rec2['group_product_id']:
-							rec['total_cost'] += rec2['total_cost']
+						if rec.get('group_product_id') == rec2.get('group_product_id'):
+							rec.get('total_cost') += rec2.get('total_cost')
 			else:
 				list_of_objects = vals['job_overhead_line_ids']
-			log.critical('job_overhead_line_ids = {}'.format(vals['job_overhead_line_ids']))
 			log.critical('list_of_objects = {}'.format(list_of_objects))
 
 		if vals.get('analytic_id'):
@@ -55,14 +55,14 @@ class job_costing_planned_amount_comparison(models.Model):
 			for record_acc in analytic_obj.product_budget_lines:
 				if analytic_obj.product_budget_lines:
 					for record_job in list_of_objects:
-						log.warning('{} == {}'.format(record_job['group_product_id'],record_acc['group_product_id'].id))
-						if record_job['group_product_id'] == record_acc['group_product_id'].id:
-							log.warning('{} > {}'.format(record_job['total_cost'],record_acc['planned_amount']))
-							if record_job['total_cost'] > record_acc['planned_amount']:
+						log.warning('{} == {}'.format(record_job.get('group_product_id'),record_acc['group_product_id'].id))
+						if record_job.get('group_product_id') == record_acc['group_product_id'].id:
+							log.warning('{} > {}'.format(record_job.get('total_cost'),record_acc['planned_amount']))
+							if record_job.get('total_cost') > record_acc['planned_amount']:
 								raise Warning('{} exceeded! {} (job cost sheet) > {} (account.analytic)'.format(record_job['group_product_id'].name,record_job['total_cost'],record_acc['total_cost']))
 				else:
 					raise Warning('No budget lines found!')
-					
+
 		res = super(job_costing_planned_amount_comparison,self).create(vals)
 		return res
 
@@ -80,8 +80,8 @@ class job_costing_planned_amount_comparison(models.Model):
 			if list_of_objects:
 				for rec in list_of_objects:
 					for rec2 in vals['job_labour_line_ids']:
-						if rec['group_product_id'] == rec2['group_product_id']:
-							rec['total_cost'] += rec2['total_cost']
+						if rec.get('group_product_id') == rec2.get('group_product_id'):
+							rec.get('total_cost') += rec2.get('total_cost')
 			else:
 				list_of_objects = vals['job_labour_line_ids']
 			log.critical('list_of_objects = {}'.format(list_of_objects))
@@ -90,8 +90,8 @@ class job_costing_planned_amount_comparison(models.Model):
 			if list_of_objects:
 				for rec in list_of_objects:
 					for rec2 in vals['job_subcon_line_ids']:
-						if rec['group_product_id'] == rec2['group_product_id']:
-							rec['total_cost'] += rec2['total_cost']
+						if rec.get('group_product_id') == rec2.get('group_product_id'):
+							rec.get('total_cost') += rec2.get('total_cost')
 			else:
 				list_of_objects = vals['job_subcon_line_ids']
 			
@@ -101,8 +101,8 @@ class job_costing_planned_amount_comparison(models.Model):
 			if list_of_objects:
 				for rec in list_of_objects:
 					for rec2 in vals['job_overhead_line_ids']:
-						if rec['group_product_id'] == rec2['group_product_id']:
-							rec['total_cost'] += rec2['total_cost']
+						if rec.get('group_product_id') == rec2.get('group_product_id'):
+							rec.get('total_cost') += rec2.get('total_cost')
 			else:
 				list_of_objects = vals['job_overhead_line_ids']
 			log.critical('list_of_objects = {}'.format(list_of_objects))
@@ -113,10 +113,10 @@ class job_costing_planned_amount_comparison(models.Model):
 			for record_acc in analytic_obj.product_budget_lines:
 				if analytic_obj.product_budget_lines:
 					for record_job in list_of_objects:
-						log.warning('{} == {}'.format(record_job['group_product_id'],record_acc['group_product_id'].id))
-						if record_job['group_product_id'] == record_acc['group_product_id'].id:
-							log.warning('{} > {}'.format(record_job['total_cost'],record_acc['planned_amount']))
-							if record_job['total_cost'] > record_acc['planned_amount']:
+						log.warning('{} == {}'.format(record_job.get('group_product_id'),record_acc['group_product_id'].id))
+						if record_job.get('group_product_id') == record_acc['group_product_id'].id:
+							log.warning('{} > {}'.format(record_job.get('total_cost'),record_acc['planned_amount']))
+							if record_job.get('total_cost') > record_acc['planned_amount']:
 								raise Warning('{} exceeded! {} (job cost sheet) > {} (account.analytic)'.format(record_job['group_product_id'].name,record_job['total_cost'],record_acc['total_cost']))
 				else:
 					raise Warning('No budget lines found!')
