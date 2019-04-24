abb5fd502824d3c7f858ce5c3c7fba710aad5c5b
diez43011 <diez43011@gmail.com>
2019-04-24 15:00:38 +0800

    Update budget_job.py

--- models/budget_job.py
+++ models/budget_job.py
@@ -112,15 +112,13 @@ class job_costing_planned_amount_comparison(models.Model):
 	def write(self,vals):
 		log.critical('vals = {}'.format(vals))
 		list_of_objects = []
-		new_vals = vals
+		new_vals = []
 
 		list_of_table_name = ['job_cost_line_ids','job_labour_line_ids','job_subcon_line_ids','job_overhead_line_ids']
 		for x in list_of_table_name:
-			num = 1
-			for record in new_vals.get(x):
+			for record in vals.get(x):
 				if record[-1] == False:
-					record.pop(num)
-				num += 1
+					new_vals.append(record)
 
 		log.critical('new_vals = {}'.format(new_vals))
 		if new_vals.get('job_cost_line_ids'):
